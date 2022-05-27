056ed3e15f2e953b348f6bbbfb33bd3c8c51f941
gs <gs>
2022-05-27 19:53:07 +0200

    first major cleanup

--- logic/fileLogic.js
+++ logic/fileLogic.js
@@ -1,106 +1,64 @@
 const fs = require('fs')
 
-const rootFolder = "./novels"
+const rootFolder = './novels'
 
-exports.UpdateNovel = (metadata) => {
-    let folderName = `${rootFolder}/${metadata.key}`
+exports.UpdateNovel = metadata => {
+  const folderName = `${rootFolder}/${metadata.key}`
 
-    if (!FolderExists(rootFolder)) {
-        try {
-            CreateFolder(rootFolder)
-        } catch (error) {
-            return
-        }
-    }
+  fs.mkdirSync(folderName, { recursive: true })
 
-    if (!FolderExists(folderName)) {
-        try {
-            CreateFolder(folderName)
-        } catch (error) {
-            return
-        }
-    }
+  const metadataJson = `${folderName}/metadata.json`
 
-    WriteMetadata(folderName, metadata)
+  if (fs.existsSync(metadataJson)) {
+    console.log('Metadata exists')
+    return
+  }
+
+  console.log(`Downloading metadata for ${metadata.title}`)
+
+  fs.writeFileSync(metadataJson, JSON.stringify(metadata))
 }
 
 exports.DownloadChapter = (chapterData, index, metaData) => {
-    let path = `${rootFolder}/${metaData.key}`
+  const path = `${rootFolder}/${metaData.key}`
+  const indexJson = `${path}/${index}.json`
+  if (fs.existsSync(indexJson)) {
+    return
+  }
 
-    if (fs.existsSync(`${path}/${index}.json`)) {
-        return
-    }
+  console.log(`Downloading chapter ${index}/${metaData.chapters} of ${metaData.title}`)
 
-    console.log(`Downloading chapter ${index}/${metaData.chapters} of ${metaData.title}`)
-
-    fs.writeFileSync(`${path}/${index}.json`, JSON.stringify(chapterData), error => {
-        if (error)
-            console.log(`Received the following error while trying to write ${metaData.title} chapter ${index}: ${error}`)
-    })
-}
-
-const FolderExists = path => {
-    return fs.existsSync(path)
-}
-
-const CreateFolder = path => {
-    try {
-        if (!fs.existsSync(path)) {
-            fs.mkdirSync(path)
-        }
-    } catch (error) {
-        console.error(`${path} does not exist, and the following error was encountered when trying to create it:\n${error}`)
-        throw error
-    }
-}
-
-const WriteMetadata = (path, metadata) => {
-    if (fs.existsSync(`${path}/metadata.json`)) {
-        console.log("Metadata exists")
-        return
-    }
-
-    console.log(`Downloading metadata for ${metadata.title}`)
-
-    fs.writeFileSync(`${path}/metadata.json`, JSON.stringify(metadata), error => {
-        if (error)
-            console.log(`Received the following error while trying to write novel metadata: ${error}`)
-    })
+  fs.writeFileSync(indexJson, JSON.stringify(chapterData))
 }
 
 exports.UpdateMetadata = metadata => {
-    console.log(`Updating metadata for ${metadata.title}`)
+  console.log(`Updating metadata for ${metadata.title}`)
 
-    fs.writeFileSync(`${rootFolder}/${metadata.key}/metadata.json`, JSON.stringify(metadata), error => {
-        if (error)
-            console.log(`Received the following error while trying to write novel metadata: ${error}`)
-    })
+  fs.writeFileSync(`${rootFolder}/${metadata.key}/metadata.json`, JSON.stringify(metadata))
 }
 
 exports.GetAllNovels = () => {
-    let folders = GetAllNovelFolders('./novels')
-    let novels = []
+  const folders = fs.readdirSync(rootFolder, { withFileTypes: true })
+  const novels = []
 
-    for (let folder of folders) {
-        let metaData = GetNovelMetaData(folder)
-        if (metaData)
-            novels.push(JSON.parse(metaData))
+  for (const folder of folders) {
+    if (!folder.isDirectory()) {
+      continue
     }
+    const metaData = GetNovelMetaData(folder)
+    if (metaData) {
+      novels.push(JSON.parse(metaData))
+    }
+  }
 
-    return novels
-}
-
-const GetAllNovelFolders = path => {
-    return fs.readdirSync(path).filter(function (file) {
-        return fs.statSync(path + '/' + file).isDirectory();
-    });
+  return novels
 }
 
 const GetNovelMetaData = key => {
-    try {
-        return fs.readFileSync(`./novels/${key}/metadata.json`, "utf8")
-    } catch (error) {
-        console.log(error)
-        return null
-    }
+  try {
+    return fs.readFileSync(`${rootFolder}/${key}/metadata.json`, 'utf8')
+  } catch (error) {
+    console.log(error)
+    return null
+  }
 }
